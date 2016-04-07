e4ad6e8568de8b7cfa8b0677983a65c1a58e7ed4
flysalmon <1023598954@qq.com>
2016-04-07 20:50:41 +0800

    flink tutorial

--- DataSet/src/main/java/enviroment/ExecutionEnvironment.java
+++ DataSet/src/main/java/enviroment/ExecutionEnvironment.java
@@ -1,38 +1,33 @@
 package enviroment;
 
-import dataset.DataSource;
-
-import java.util.Arrays;
-import java.util.Collection;
-
 /**
  * Created by Acceml on 2016/3/31
  * Email: huminghit@gmail.com
  */
 public class ExecutionEnvironment {
-
-    private static ExecutionEnviromentFactory contextEnvironmentFactory;
-
-    private ExecutionEnvironment() {
-
-    }
-
-    public static ExecutionEnvironment getExecutionEnvironment() {
-        return contextEnvironmentFactory == null ?
-                new ExecutionEnvironment() : contextEnvironmentFactory.createExecutionEnvironment();
-    }
-
-    public final <T> DataSource<T> fromCollection(Collection<T> data) {
-        return new DataSource<T>(data);
-    }
-
-    public final <T> DataSource<T> fromElement(T... data) {
-        if (data == null) {
-            throw new IllegalArgumentException("the data must not be null.");
-        }
-        if (data.length == 0) {
-            throw new IllegalArgumentException("the number of elements must not be null");
-        }
-        return fromCollection(Arrays.asList(data));
-    }
+//
+//    private static ExecutionEnviromentFactory contextEnvironmentFactory;
+//
+//    private ExecutionEnvironment() {
+//
+//    }
+//
+//    public static ExecutionEnvironment getExecutionEnvironment() {
+//        return contextEnvironmentFactory == null ?
+//                new ExecutionEnvironment() : contextEnvironmentFactory.createExecutionEnvironment();
+//    }
+//
+//    public final <T> DataSource<T> fromCollection(Collection<T> data) {
+//        return new DataSource<T>(data);
+//    }
+//
+//    public final <T> DataSource<T> fromElement(T... data) {
+//        if (data == null) {
+//            throw new IllegalArgumentException("the data must not be null.");
+//        }
+//        if (data.length == 0) {
+//            throw new IllegalArgumentException("the number of elements must not be null");
+//        }
+//        return fromCollection(Arrays.asList(data));
+//    }
 }
